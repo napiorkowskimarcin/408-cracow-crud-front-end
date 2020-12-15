1e5e56b057f41142eba62343a5d93935e9f57b9e
Marcin Napiorkowski <napiorkowski.marcin.gmail.com>
2020-12-15 14:33:29 +0100

    JWT START

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
+//
 import router from "./router";
 import App from "./App.vue";
 import store from "./store";
+//
 import axios from "axios";
 import VueAxios from "vue-axios";
 
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,16 +3,36 @@
     Welcome Please log in
     <div><router-link to="/login">LOGIN PAGE</router-link></div>
     <div><router-link to="/signup">SIGN UP PAGE</router-link></div>
+    <div class="get-users">
+      <h3>List of existing users:</h3>
+      <ul>
+        <li v-for="(item, index) in list" :key="index">
+          <!--  -->
+          {{ item }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Welcome",
+  data() {
+    return {
+      list: undefined,
+    };
+  },
+  mounted() {
+    this.axios.get("http://localhost:3000/api/users").then((res) => {
+      this.list = res.data;
+    });
+  },
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-/*  */
+<style lang="scss">
+ul {
+  list-style-type: none;
+}
 </style>
