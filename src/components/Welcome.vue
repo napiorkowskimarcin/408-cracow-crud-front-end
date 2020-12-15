9f32e141cd2c226b3a9de64909934558e7b9ac66
Marcin Napiorkowski <napiorkowski.marcin.gmail.com>
2020-12-15 06:44:52 +0100

    INIT

---
+++ src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    Welcome
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/*  */
+</style>
